99ae68c6e59b8f9b916b38bbe094d73c286ff2ea
haistler <haistler@ukr.net>
2017-06-25 14:54:59 +0300

    Painting service is done

--- src/app/shared/painting.service.ts
+++ src/app/shared/painting.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
 import {Painting} from '../entites/painting';
 import {AbstractService} from './abstract.service';
+import {Category} from '../entites/category';
 
 @Injectable()
 export class PaintingService extends AbstractService {
 
   public getPaintings(limit: number, offset: number,
-                      categoryId: number): Promise<Painting[]> {
-
-    return null;
+                      categoryId?: number): Promise<Painting[]> {
+    let url: string = this.baseUrl + `getByPages/${limit}/${offset}/`;
+    if (categoryId) {
+      url += categoryId;
+    }
+    return this.httpService.get(url)
+      .then(response => {
+        const paintings: Array<Painting> = [];
+        for (const item of response){
+          const category: Category = new Category(item.id_category);
+          paintings.push(
+            new Painting(item.id_painting, item.name,
+              item.description, item.image_url, category)
+          );
+        }
+        return Promise.resolve(paintings);
+      })
+      .catch(error => {
+        return Promise.reject(error);
+      });
   }
 }
